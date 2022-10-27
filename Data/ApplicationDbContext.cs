5033653395abdc3e9f1316fc5da6745bb87959a2
Quinn Matthews <Quinn.matthews@lrs.com>
2022-10-27 12:18:04 -0500

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LRSIceRink.Data {
+    public class ApplicationDbContext : IdentityDbContext {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) {
+        }
+    }
+}

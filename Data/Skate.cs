96826e29d2581864b7b7078376b699088e709ebe
Quinn Matthews <Quinn.matthews@lrs.com>
2022-10-31 18:47:55 -0500

    Add Skate Archive property

--- Data/Skate.cs
+++ Data/Skate.cs
@@ -10,6 +10,7 @@
         public string Name { get; set; }
         public SkateStatus Status { get; set; }
         public double MinutesUsed { get; set; }
+        public bool IsArchived { get; set; }
         public virtual List<SkateUsage> SkateUsages { get; set; }
         public virtual List<AuditLog> SkateHistory { get; set; }
         public void addMinutesUsed(double minutes)
---
+++ Migrations/20221031234656_AddSkateArchiveStatus.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LRSIceRink.Migrations
+{
+    public partial class AddSkateArchiveStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsArchived",
+                table: "Skates",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsArchived",
+                table: "Skates");
+        }
+    }
+}
